3fcce62581ab630bedc896803d1cd6aa84112ef9
Carlos Eduardo Valdez Quintín <hi@carlosvq.xyz>
2018-11-05 08:03:30 -0400

    Revert "Cesar"

--- src/components/styles/Select/Select.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import {containerProps, fontProps} from '../Properties/Properties';
-
-const Select = styled.select`
-    ${containerProps}
-    ${fontProps}
-`;
-
-export default Select;
--- src/components/styles/Span/Span.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import {fontProps} from '../Properties/Properties';
-
-const Span = styled.span`
-    ${fontProps}
-`;
-
-export default Span;
