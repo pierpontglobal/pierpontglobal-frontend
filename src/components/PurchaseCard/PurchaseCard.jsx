61e11190adc649ade578e80a53c0003cea9c7177
Daniel Pena Berroa <digrape07@gmail.com>
2019-05-01 11:06:11 -0400

    Card bid in purchases resposnive

--- src/components/PurchaseCard/PurchaseCard.jsx
+++ src/components/PurchaseCard/PurchaseCard.jsx
@@ -29,53 +29,53 @@ export default class PurchaseCard extends React.Component {
 
         const {open} = this.state;
         return (
+          <Container
+            className="mb-3" 
+            backgroundColor="#fafafa"
+            boxShadow="0 1px 2px 0 rgba(0, 0, 0, 0.18)"
+          >
             <Container
-                className="mb-3" 
-                backgroundColor="#fafafa"
-                boxShadow="0 1px 2px 0 rgba(0, 0, 0, 0.18)"
+              className="d-flex flex-row justify-content-between pl-3 pr-4"
+              height="70px"
+              hoverCursor="pointer"
+              onClick={this.toggleCard}
             >
-                <Container
-                    className="d-flex flex-row justify-content-between pl-3 pr-4"
-                    height="70px"
-                    hoverCursor="pointer"
-                    onClick={this.toggleCard}
+              <div>
+                <Text
+                  className="mb-0 pt-3"
+                  opacity={0.87}
+                  fontWeight={600}
+                  lineHeight={1.25}
                 >
-                    <div>
-                        <Text
-                            className="mb-0 pt-3"
-                            opacity={0.87}
-                            fontWeight={600}
-                            lineHeight={1.25}
-                        >
-                            {car}
-                        </Text>
-                        <Text
-                            className="mb-0"
-                            opacity={0.54}
-                            fontSize="0.75em"
-                            lineHeight={1.67} 
-                        >
-                            {`Order Number: ${orderNumber}`}
-                        </Text>
-                    </div>
-                    <div className="d-flex flex-column justify-content-center">
-                        <i 
-                            className={`fas fa-chevron-${open ? 'up': 'down'}`}
-                            style={iconColor} 
-                        />
-                    </div>
-                </Container>
-                <Collapse isOpen={open}>
-                    {steps.map((s, i) => 
-                        <Step 
-                            key={i}
-                            number={i + 1}
-                            date={s.date}
-                            completed={s.completed}
-                            text={s.text}
-                        />)}
-                </Collapse>
+                  {car}
+                </Text>
+                <Text
+                  className="mb-0"
+                  opacity={0.54}
+                  fontSize="0.75em"
+                  lineHeight={1.67} 
+                >
+                  {`Order Number: ${orderNumber}`}
+                </Text>
+              </div>
+              <div className="d-flex flex-column justify-content-center">
+                <i 
+                  className={`fas fa-chevron-${open ? 'up': 'down'}`}
+                  style={iconColor} 
+                />
+              </div>
             </Container>
+            <Collapse isOpen={open}>
+              {steps.map((s, i) => 
+                <Step 
+                  key={i}
+                  number={i + 1}
+                  date={s.date}
+                  completed={s.completed}
+                  text={s.text}
+                />)}
+            </Collapse>
+          </Container>
         );
     }
 }
