f67a4bdeac4d51ad827d517eb4ff5a64c9f2a60c
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-05-04 09:31:53 -0400

    Notification subscription strategy changed

--- src/components/AccountPanel/AccountPanel.jsx
+++ src/components/AccountPanel/AccountPanel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import { withCookies } from 'react-cookie';
-import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import Tab from './Tab/Tab';
 import DealerTab from './DealerTab/DealerTab';
