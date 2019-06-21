c8a237be6b271396998f448d82f10417f42fe4dc
Daniel Pena Berroa <digrape07@gmail.com>
2019-06-21 14:13:42 -0400

    fixed add deposit modal content

--- src/components/pages/ProfilePage/SettingSide/Components/Modals/AddDeposit.jsx
+++ src/components/pages/ProfilePage/SettingSide/Components/Modals/AddDeposit.jsx
@@ -61,6 +61,7 @@ const DepositForm = styled.div`
   justify-content: space-around;
   align-items: center;
   flex-direction: row;
+  flex-wrap: wrap;
   @media only screen and (max-width: 768px) {
     flex-direction: column;
   }
